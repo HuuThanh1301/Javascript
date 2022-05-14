acada19a5968cb1bb28fb01723e26e6a6fa85af1
HuuThanh1301 <nhthanhdn95@gmail.com>
2022-05-14 15:18:28 +0700

    add Day1

---
+++ Day-1/index.js
@@ -0,0 +1,20 @@
+var myVar = "Meo Meo";
+function myFunc1(){
+    document.getElementById("animal").innerText = variable;
+}
+
+function myFunc2(){
+    var yourName= prompt("Please enter your name");
+    if(yourName != null){
+        alert("Hello " + yourName);
+    }
+}
+
+function myFunc3(){
+    var name= 'user';
+    var number = 10023;
+    var arr= [1,2,3];
+    console.log(name);
+    console.log(number);
+    console.log(arr);
+}
