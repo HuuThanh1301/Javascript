dcf904242fe547d2cd0356e7f6bfb8807f4a55c6
HuuThanh1301 <nhthanhdn95@gmail.com>
2022-07-14 17:16:15 +0700

    Ai la trieu phu version full chuc nang

--- Day-14 (Test)/index.js
+++ Day-14 (Test)/index.js
@@ -1,8 +1,14 @@
-const questionList = ['Ai dep trai nhat lop?', 'Con gi co 4 chan?', 'Con gi biet bay?' ];
+const questionList = ['Ai dep trai nhat lop?', 'Con gi co 4 chan?', 'Con gi biet bay?', '1 + 1 bang bao nhieu?', 'Nuoc nao dong dan nhat the gioi?'];
 
-const answerList = [{a: 'Loc', b: 'Toan', c: 'Thuan', d: 'Vu'}, {a: 'ga', b: 'cho', c: 'vit', d: 'ran'}, {a: 'heo', b: 'khi', c: 'chim', d: 'meo'}];
+const answerList = [
+    {a: 'Loc', b: 'Toan', c: 'Thuan', d: 'Vu'},
+    {a: 'ga', b: 'cho', c: 'vit', d: 'ran'},
+    {a: 'heo', b: 'khi', c: 'chim', d: 'meo'},
+    {a:'3', b: '4', c: '5', d: '2'},
+    {a:'My', b: 'An Do', c: 'Indonesia', d: 'Trung Quoc'}
+];
 
-const correctAnswer = ['Loc', 'cho', 'chim'];
+const correctAnswer = ['Loc', 'cho', 'chim', '2', 'Trung Quoc'];
 
 const $ = (x) => document.querySelector(x);
 const $All = (x) => document.querySelectorAll(x);
@@ -33,11 +39,14 @@ displayQuestion(0);
 displayAnswer(0);
 
     let i = 0;
-    const max_question = 3;
+    const max_question = 5;
     const choiceList = $All('div[name="choice"]');
     [...choiceList].forEach(element => {
         element.addEventListener('click', e => {
             if(e.target.innerHTML === correctAnswer[i]){
+                if($('#fifty').style.display === 'none'){
+                    [...$All('div[name="choice"]')].forEach(item => item.style.display = 'block');
+                }
                 if(i != max_question - 1){
                     displayQuestion(i+1);
                     displayAnswer(i+1);
@@ -54,11 +63,32 @@ displayAnswer(0);
         })
     }); 
 
+    $('#skip').onclick = (e) => {
+        e.preventDefault();
+        $('#skip').style.display = 'none';
+        displayQuestion(i+1);
+        displayAnswer(i+1);
+        i+=1;
+    }
+
+    $('#fifty').onclick = (e) => {
+        e.preventDefault();
+        // console.log('haha', $('#fifty').style.display);
+        console.log(correctAnswer[i]);
+        const wrongAnswer = [...$All('div[name="choice"]')].filter(item => item.innerHTML !== correctAnswer[i]);
+        const shuffled_wrongAnswer = wrongAnswer.sort(() => 0.5 - Math.random());
+        shuffled_wrongAnswer.slice(0, 2).forEach(item => item.style.display = 'none');
+        $('#fifty').style.display = 'none';
+
+        console.log(shuffled_wrongAnswer);
+    }
+
     $('#next').onclick = (e) => {
         e.preventDefault();
         if($('#playername').value !== ''){
             $('#modal').style.display = 'none';
             $('#layout').style.display = 'block';
+            $('#help').style.display = 'flex';
             alert('Chao mung ' + $('#playername').value + ' da den voi ai la trieu phu!');
         }
     }
